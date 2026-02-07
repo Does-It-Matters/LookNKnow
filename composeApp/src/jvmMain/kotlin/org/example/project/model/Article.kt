a3b944d6071379afd513a206c3809cfc0dd87853
glass <silver20190204@gmail.com>
2026-02-07 17:16:35 +0900

    add: article 주요 모델 추가

---
+++ composeApp/src/jvmMain/kotlin/org/example/project/model/Article.kt
@@ -0,0 +1,10 @@
+package org.example.project.model
+
+data class Article(
+    val title: String,
+    val mainImgUri: String?,
+    val paragraphs: List<Paragraph>,
+    val author: String,
+    val createdAt: String,
+    val updatedAt: String,
+)
---
+++ composeApp/src/jvmMain/kotlin/org/example/project/model/Paragraph.kt
@@ -0,0 +1,7 @@
+package org.example.project.model
+
+data class Paragraph(
+    val subTitle: String,
+    val imageUri: String?,
+    val text: String,
+)
